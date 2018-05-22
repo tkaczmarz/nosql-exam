ecbc6785b18106742b38d7dcb2b9e83a729b7989
tkaczmarz <kaczmarz.tomasz@gmail.com>
2018-05-22 21:57:36 +0200

    Dataset aggregation with time.

---
+++ app/arrested-thefts.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+import pprint
+import time
+
+client = MongoClient("mongodb://localhost:27017")
+db = client.test
+
+# aggregate
+pipeline = [
+    {'$match': {'Arrest': 'True'}},
+    {'$group': {'_id': '$Primary Type', 'arrests': {'$sum': 1}}},
+    {'$sort': {'arrests': -1}}
+]
+start_time = time.time()
+result = list(db.crimes.aggregate(pipeline))
+exec_time = time.time() - start_time
+
+pprint.pprint(result)
+
+print("execution time: " + str(exec_time) + " seconds")
