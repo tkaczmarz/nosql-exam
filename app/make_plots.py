bf5d3d11d39e93e23072490998708e8546433140
Piotr Kawa <piotr.kawa@intel.com>
2018-05-23 13:33:12 +0200

    test

--- app/make_plots.py
+++ app/make_plots.py
@@ -8,7 +8,7 @@ def read_data(filename, keys=["Primary_Type", "value"]):
         for line in file:
             values = line.split(",")
             documents[0].append(values[0])
-            documents[1].append(values[1])
+            documents[1].append(int(values[1].split(".")[0]))
         return documents
 
 
@@ -16,7 +16,7 @@ def generate_bar_chart(filename):
     plt.bar(data[0], data[1])
     plt.xlabel("Type of crime")
     plt.ylabel("Amount of crimes commited")
-    plt.savefig("/app/static/{}.png".format(filename))
+    plt.savefig("./static/{}.png".format(filename))
 
 
 def generate_charts():
