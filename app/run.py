e21e6ebc966b207757fb1aab759a58f2bddfcf95
Piotr Kawa <piotr.kawa@intel.com>
2018-05-23 13:58:53 +0200

    dess

--- app/run.py
+++ app/run.py
@@ -3,7 +3,8 @@ if __name__ == "__main__":
 
     os.system(
         "mongoimport --host mongodb_database --port 27017 -d test -c crimes --type csv --file /tmp/Chicago_Crimes_2001_to_2004.csv --headerline && rm /tmp/crimes.zip")
-    from . import make_plots, crimes_aggr, crimes_mr, not_arrested, app
+    # from . import make_plots, crimes_aggr, crimes_mr, not_arrested, app
+    from app import not_arrested, crimes_aggr, crimes_mr, app, make_plots
     crimes_mr.map_reduce("mongodb_database")
     crimes_aggr.aggregattion("mongodb_database")
     not_arrested.not_arrested("mongodb_database")
