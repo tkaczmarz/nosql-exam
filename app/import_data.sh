90e6e42fe52292dfcfe178491aa80c3e2ba6d3e1
Piotr Kawa <piotr.kawa@intel.com>
2018-05-23 10:02:53 +0200

    hotfix 2.0

--- app/import_data.sh
+++ app/import_data.sh
@@ -1,4 +1,1 @@
-#!/usr/bin/env bash
-
-mongoimport --host mongodb_database --port 27017 -d test -c crimes --type csv --file /tmp/Chicago_Crimes_2001_to_2004.csv --headerline
-rm /tmp/crimes.zip
+mongoimport --host mongodb_database --port 27017 -d test -c crimes --type csv --file /tmp/Chicago_Crimes_2001_to_2004.csv --headerline && rm /tmp/crimes.zip
